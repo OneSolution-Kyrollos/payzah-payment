b6f2f34751537d25e45f58237a8dc4d2bf2bf0dd
OneSolution-Kyrollos <kzaki@onesolutionc.com>
2026-05-07 18:10:48 +0300

    Initial commit: Payzah Payment Gateway v17.0.1.0.0

---
+++ payzah_payment_gateway/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Payzah Payment Gateway',
+    'summary': "Payzah Payment Gateway for Odoo 17",
+    'description': """
+Payzah Payment Gateway Integration for Odoo 17
+Supports Knet and Credit Card payments.
+Developed by One Solutions - Kuwait
+Contact: +96592229650
+    """,
+    'author': "Kyrollos Zaki | +96592229650",
+    'company': 'One Solutions',
+    'maintainer': 'Kyrollos Zaki | +96592229650',
+    'website': "https://www.onesolutionc.com",
+    'license': 'LGPL-3',
+    'category': 'Accounting/Payment Acquirers',
+    'version': '17.0.1.0.0',
+    'depends': [
+        'payment',
+        'account',
+        'website',
+        'website_sale',
+    ],
+    'data': [
+        'views/payment_provider_view.xml',
+        'views/payment_payzah_templates.xml',
+        'views/payzah_payment_template.xml',
+        'data/payment_provider_data.xml',
+    ],
+    'images': ['static/description/images.png'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ payzah_payment_gateway/controllers/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import payzah_payment_gateway
